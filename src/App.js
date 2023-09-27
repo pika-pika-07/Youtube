c78b8664e8b52450e241bb468b0e98f45655d73a
Pika-Pika-07 <parthbhatnagar40@gmail.com>
2023-09-27 15:24:23 +0530

    Added basic grid layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
 import "./App.css";
+import Body from "./components/Body";
+import Header from "./components/Header";
 const App = () => {
-  return <div className="bg-red font-bold text-3xl">Youtube</div>;
+  return (
+    <div className="bg-red font-bold text-3xl">
+      {/*
+      Header
+      Body
+        SideBar
+          MenuItems
+        MainContainer
+          ButtonsList
+          VideoContainer
+            VideoCard  
+      */}
+
+      <Header />
+      <Body />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      {/* Left section */}
+      <div className="flex col-span-1">
+        <img
+          className="h-12"
+          alt="menu"
+          src="https://static.vecteezy.com/system/resources/thumbnails/002/292/406/small/hamburger-menu-line-icon-free-vector.jpg"
+        />
+        <img
+          className="h-14"
+          alt="youtube logo"
+          src="https://i0.wp.com/www.dafontfree.io/wp-content/uploads/2021/08/Youtube-Logo-Font.jpg?resize=849%2C395&ssl=1"
+        />
+      </div>
+      {/* // Middle section */}
+      <div className="flex col-span-10 justify-center">
+        <input
+          type="text"
+          className="w-1/2 border border-gray-400 h-11 rounded-l-full p-2"
+        />
+        <button className="border border-gray-400 h-11 rounded-r-full px-6 bg-gray-100">
+          {" "}
+          🔎
+        </button>
+      </div>
+
+      {/* Right section */}
+      <div className="flex col-span-1">
+        <img
+          className="h-12"
+          src="https://cdn-icons-png.flaticon.com/512/666/666201.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
