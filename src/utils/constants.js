aba1b8208591d9048bfc12924803bebcac510751
Pika-Pika-07 <parthbhatnagar40@gmail.com>
2023-10-01 21:48:14 +0530

    cors fixed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,6 @@
 const GOOGLE_API_KEY = "AIzaSyCZxvNGjt8Rm_9hEck3MUaL6Nx46iBTvGo";
 export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=${GOOGLE_API_KEY}`;
-export const YOUTUBE_SEARCH_API =
-  "https://suggestqueries.google.com/complete/search?client=chrome&ds=yt&q=";
+export const YOUTUBE_SEARCH_API = "/complete/search?client=chrome&ds=yt&q=";
 export const YOUTUBE_VIDEOS_SEARCH_API = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=${GOOGLE_API_KEY}`;
 
 export const OFFSET_LIVE_CHAT = 25;
