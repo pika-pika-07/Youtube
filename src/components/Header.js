fb006b85cc4954d44129be06c40e3edd00752b61
Pika-Pika-07 <parthbhatnagar40@gmail.com>
2023-10-02 10:17:02 +0530

    cors fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -67,8 +67,9 @@ const Header = () => {
      */
 
     // If search query present , dont make api call
-
-    const url = `${YOUTUBE_SEARCH_API}${searchQuery}`;
+    const test =
+      "https://www.google.com/complete/search?client=chrome&ds=yt&q=%";
+    const url = `/cors-proxy/${YOUTUBE_SEARCH_API}${searchQuery}`;
 
     const data = await fetch(url, {
       // mode: "cors",
