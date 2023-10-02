1640dd4fdf6b4704964df388d37115a4341aea12
Pika-Pika-07 <parthbhatnagar40@gmail.com>
2023-10-02 09:29:12 +0530

    cors fixed

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,15 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+// [[redirects]]
+//   from = "http://suggestqueries.google.com/complete/search?client=chrome&ds=yt&q="
+//   to = "http://suggestqueries.google.com/complete/search?client=chrome&ds=yt&q="
+//   status = 200
+//   force = true
+//   headers = {X-From = "Netlify"}
+// [[headers]]
+//   for = "/*"
+//   [headers.values]
+//     Access-Control-Allow-Origin = "*"
